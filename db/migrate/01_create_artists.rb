179bb523911a3aa93a318e4811b980b1383a48da
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-27 22:32:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,11 +8,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 
   def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
+    # create_table :artists do |t|
+    #   t.string :name
+    #   t.string :genre
+    #   t.integer :age
+    #   t.string :hometown
+    # end
   end
 end
